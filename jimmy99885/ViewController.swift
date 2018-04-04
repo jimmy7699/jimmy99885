984f5610ae6c8908fe44dcd86ac835352fe9f3b2
邱錦華 <mac@macde-macbook-pro.local>
2018-04-04 15:15:01 +0800

    Initial Commit

---
+++ jimmy99885/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jimmy99885
+//
+//  Created by 邱錦華 on 2018/4/4.
+//  Copyright © 2018年 jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
